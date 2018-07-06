4df20d57badffda99566cb6deec1f9aa9bc0bc1e
khbradley <40898481+khbradley@users.noreply.github.com>
2018-07-06 15:07:11 -0400

    Update index.js

--- townsim/index.js
+++ townsim/index.js
@@ -1,4 +1,27 @@
+/*
+Start State
+
+Play State
+
+Game Over State
+
+*/
+
 var lastUpdate;
+
+function SpriteSheet(path, frameWidth, frameHeight) {
+ 
+   var image = new Image();
+   var framesPerRow;
+ 
+   // calculate the number of frames in a row after the image loads
+   var self = this;
+   image.onload = function() {
+      framesPerRow = Math.floor(image.width / frameWidth);
+   };
+ 
+   image.src = "images/PathAndObjects.png";
+}
 
 function tick() {
   var now = window.Date.now();
